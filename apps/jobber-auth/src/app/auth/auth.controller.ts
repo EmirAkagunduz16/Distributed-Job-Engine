8213e3d8cb9d4054bba4b44698279e505e0c56d0
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-01-21 11:31:07 +0300

    upd

---
+++ apps/jobber-auth/src/app/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import {
+  AuthenticateRequest,
+  AuthServiceController,
+  AuthServiceControllerMethods,
+  User,
+} from '@jobber/types/proto/auth';
+import { Controller, UseGuards } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { UsersService } from '../users/users.service';
+import { TokenPayload } from './token-payload.interface';
+
+@Controller()
+@AuthServiceControllerMethods()
+export class AuthController implements AuthServiceController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  authenticate(
+    request: AuthenticateRequest & { user: TokenPayload },
+  ): Promise<User> | Observable<User> | User {
+    return this.usersService.getUser({ id: request.user.userId });
+  }
+}
--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -3,9 +3,26 @@ import { FibonacciJob } from './fibonacci.job';
 import { DiscoveryModule } from '@golevelup/nestjs-discovery';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AUTH_PACKAGE_NAME } from '@jobber/types/proto/auth';
+import { join } from 'path';
+import { PulsarModule } from '@jobber/pulsar';
 
 @Module({
-  imports: [DiscoveryModule],
+  imports: [
+    DiscoveryModule,
+    PulsarModule,
+    ClientsModule.register([
+      {
+        name: AUTH_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: AUTH_PACKAGE_NAME,
+          protoPath: join(__dirname, 'proto/auth.proto'),
+        },
+      },
+    ]),
+  ],
   providers: [FibonacciJob, JobsService, JobsResolver],
 })
 export class JobsModule {}
