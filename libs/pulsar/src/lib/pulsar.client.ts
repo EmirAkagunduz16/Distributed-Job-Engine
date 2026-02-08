5a6cfc3a4376e5235fe3bff76459818a8243131c
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-02-08 13:20:06 +0300

    upd

--- libs/pulsar/src/lib/pulsar.client.ts
+++ libs/pulsar/src/lib/pulsar.client.ts
@@ -14,7 +14,9 @@ export class PulsarClient implements OnModuleDestroy {
   constructor(private readonly configService: ConfigService) {}
 
   async createProducer(topic: string) {
-    const producer = await this.client.createProducer({ topic });
+    const producer = await this.client.createProducer({
+      topic,
+    });
     this.producers.push(producer);
     return producer;
   }
